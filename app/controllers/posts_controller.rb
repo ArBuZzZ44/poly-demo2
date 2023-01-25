6e1129508d559f953a42bd861b9036891ffe43b3
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-25 14:27:55 +0700

    add create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to posts_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
