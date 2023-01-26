f46af99ff3c789eabb74343235fe35714cb035c6
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-27 00:51:46 +0700

    add index/new/create methods

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,23 @@
 class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create 
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to images_path
+    else
+      render action: 'new'
+    end
+
+    private
+
+    def image_params
+      params.require(:image).permit(:image, :title)
+    end
 end
